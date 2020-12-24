fb9533db6080cb585934b91698228f56d610fa9d
Jeff Bai <j.bai@wustl.edu>
2020-12-24 09:55:36 +0000

    Automatically backed up by Learn

--- lib/min.rb
+++ lib/min.rb
@@ -11,9 +11,5 @@ require 'pry'
 def get_the_min(groceries)
   #code your solution here!
   groceries.each do |key, value|
-    if key == "vegetable"
-      key.each do |item|
-      end
-    end
   end
 end
